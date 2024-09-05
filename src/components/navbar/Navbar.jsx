a8871eb7aea575d90a61cbe4c6b0ec14a2649251
aizarfarhann <aizarfarhann@gmail.com>
2024-09-06 01:32:46 +0700

    Add files via upload

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import Sidebar from "../sidebar/Sidebar"
+import "./navbar.scss"
+import {motion} from "framer-motion"
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            {/* Sidebar */}
+            <Sidebar/>
+            <div className="wrapper">
+                <motion.span 
+                initial={{opacity: 0, scale: 0.5}} 
+                animate={{opacity: 1, scale: 1}} 
+                transition={{duration: 0.5}}
+                >
+                    Aizar Farhan
+                </motion.span>
+                <div className="social">
+                    <a href="#"><img src="/linkedin.png" alt=""/></a>
+                    <a href="#"><img src="/instagram2.png" alt=""/></a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
